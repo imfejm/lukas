2e10a99109e18550536e4584c727f2092c8c4c61
imfejm <4imfejm@gmail.com>
2025-02-14 14:50:16 +0100

    grid

---
+++ script.js
@@ -0,0 +1,13 @@
+const tlacitko = document.querySelector("#ham");
+
+tlacitko.addEventListener("click", () => {
+  const rozbal = document.querySelector("#menu");
+  rozbal.classList.toggle("hidden");
+  const cara2 = document.querySelector("#cara2");
+  cara2.classList.toggle("caraB");
+
+  const cara1 = document.querySelector("#cara1");
+  cara1.classList.toggle("caraA");
+  const cara3 = document.querySelector("#cara3");
+  cara3.classList.toggle("caraC");
+});
