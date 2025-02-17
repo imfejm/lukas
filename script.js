28f97dee6f252e01687aecc348e4574ceaba7bbf
imfejm <4imfejm@gmail.com>
2025-02-17 23:38:37 +0100

    h2 effect for mobile

--- script.js
+++ script.js
@@ -45,3 +45,20 @@ function autoChange() {
 
 setInterval(autoChange, 5000); // Automatická změna každých 5 sekund
 changeImage(currentIndex); // Nastavení prvního obrázku
+
+//hover nadpisu na img odkazu mobilni verze
+document.addEventListener("DOMContentLoaded", function () {
+  const images = document.querySelectorAll(".image-wrapper");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.querySelector("h2").classList.add("show");
+      } else {
+        entry.target.querySelector("h2").classList.remove("show");
+      }
+    });
+  }, { threshold: 0.5 }); // Spustí se, když je 50 % prvku viditelné
+
+  images.forEach(image => observer.observe(image));
+});
